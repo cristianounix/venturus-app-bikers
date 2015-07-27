ea8bdabfb8d1cca05a31f7051e4433267596e2a2
cristianounix <cristianounix@gmail.com>
2015-07-27 03:31:46 -0300

    Adicionando concat para melhorar a minificação do arquivo final

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,18 @@ module.exports = function(grunt) {
     jshint: {
       files: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js'],
     },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['js/app.js', 'js/widgets.js'],
+        dest: 'js/all.js',
+      },
+    },
     uglify: {
       build: {
-        src: 'js/*.js',
+        src: 'js/all.js',
         dest: 'js/app.min.js'
       }
     },
@@ -23,10 +32,11 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-stylus');
 
-  grunt.registerTask('default', ['stylus', 'uglify']);
+  grunt.registerTask('default', ['concat','stylus', 'uglify']);
 
   grunt.registerTask('build', ['default']);
 };
